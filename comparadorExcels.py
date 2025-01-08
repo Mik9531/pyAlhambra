dfcf5cbc9f4922c694469877a61242f0692d7206
Miguel Afán Espinosa <miguelafannn@gmail.com>
2025-01-08 13:54:21 +0100

    v3 del comparador, con pasaporte en coreano

--- comparadorExcels.py
+++ comparadorExcels.py
@@ -4,6 +4,7 @@ from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
 from datetime import datetime
 
+
 def find_header_row(file, keyword):
     """
     Encuentra la fila donde se encuentra una columna que contiene el keyword.
@@ -14,6 +15,7 @@ def find_header_row(file, keyword):
         if any(keyword.lower() in (str(cell).lower() if cell else "") for cell in row):
             return i
     return 1  # Por defecto, si no se encuentra, usar la primera fila
+
 
 def compare_excels(file1, file2, output_file):
     # Detectar la fila de encabezados en ambos archivos
@@ -29,15 +31,15 @@ def compare_excels(file1, file2, output_file):
     df2.columns = [f"Col_{i}" if 'Unnamed' in col else col for i, col in enumerate(df2.columns)]
 
     # Identificar columnas de interés
-    cols_of_interest1 = [col for col in df1.columns if any(keyword in col.lower() for keyword in ["name", "nombre", "pass","영문명"]) and "kor" not in col.lower()]
-    cols_of_interest2 = [col for col in df2.columns if any(keyword in col.lower() for keyword in ["name", "nombre", "pass","영문명"]) and "kor" not in col.lower()]
+    cols_of_interest1 = [col for col in df1.columns if any(
+        keyword in col.lower() for keyword in ["name", "nombre", "pass", "영문명", "영문성함","여권번호"]) and "kor" not in col.lower()]
+    cols_of_interest2 = [col for col in df2.columns if any(
+        keyword in col.lower() for keyword in ["name", "nombre", "pass", "영문명","영문성함","여권번호"]) and "kor" not in col.lower()]
 
-    # cols_of_interest = ["name", "nombre", "pass"]
-    # Verificar que ambas tablas tienen las columnas de interés
-
-    # missing_cols = [col for col in cols_of_interest if col not in str(df2.columns).lower()]
-    # if missing_cols:
-    #     raise ValueError(f"Faltan columnas requeridas en el archivo 2: {', '.join(missing_cols)}")
+    # if len(cols_of_interest1) == 0 or len(cols_of_interest2) == 0:
+    #     result_label.config(text="No se encuentran las columnas de nombre o pasaporte en los archivos añadidos",
+    #                         fg="red")
+    #     return
 
     # Filtrar ambas tablas
     df1_filtered = df1[cols_of_interest1]
@@ -53,8 +55,8 @@ def compare_excels(file1, file2, output_file):
         if row < len(df2_filtered):
             for i in range(len(cols_of_interest1)):
                 if df1_filtered.at[row, cols_of_interest1[i]] != df2_filtered.at[row, cols_of_interest2[i]]:
-                    diff_df.at[row, cols_of_interest1[i]] = f'{df1_filtered.at[row, cols_of_interest1[i]]} --> {df2_filtered.at[row, cols_of_interest2[i]]}'
-
+                    diff_df.at[row, cols_of_interest1[
+                        i]] = f'{df1_filtered.at[row, cols_of_interest1[i]]} --> {df2_filtered.at[row, cols_of_interest2[i]]}'
 
     # Guardar diferencias en un nuevo archivo
     diff_df.to_excel(output_file, index=False)
@@ -75,13 +77,13 @@ def compare_excels(file1, file2, output_file):
     wb.save(output_file)
 
 
-
 def browse_file(label):
     filename = filedialog.askopenfilename(filetypes=[["Excel files", "*.xlsx"]])
     if filename:
         label.config(text=filename)
         return filename
     return None
+
 
 def run_comparator():
     file1 = label_file1.cget("text")
@@ -97,6 +99,7 @@ def run_comparator():
         result_label.config(text=f"Comparación completada. Archivo guardado como: {output_file}", fg="green")
     except ValueError as e:
         result_label.config(text=str(e), fg="red")
+
 
 # Crear la interfaz gráfica
 root = Tk()
